f9bdc7031b6c2825505082478d812486f7144d43
Matthias Siegmund <matzesiegmund@web.de>
2017-01-30 22:21:29 +0100

    Remove deserializer

--- app/src/main/java/com/siegmund/cleanarchitectureexample/dagger/GsonModule.kt
+++ app/src/main/java/com/siegmund/cleanarchitectureexample/dagger/GsonModule.kt
@@ -2,8 +2,6 @@ package com.siegmund.cleanarchitectureexample.dagger
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.siegmund.cleanarchitectureexample.api.MovieResponseDeserializer
-import com.siegmund.cleanarchitectureexample.api.MovieResponse
 import dagger.Module
 import dagger.Provides
 
@@ -11,6 +9,6 @@ import dagger.Provides
 class GsonModule {
     @Provides
     fun provideGson(): Gson {
-        return GsonBuilder()/*.registerTypeAdapter(MovieResponse::class.java, MovieResponseDeserializer())*/.create()
+        return GsonBuilder().create()
     }
 }

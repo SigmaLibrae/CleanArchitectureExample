9e0db15f68982b39d48263e4a399186e83e6c0de
Matthias Siegmund <matzesiegmund@web.de>
2017-02-07 22:30:04 +0100

    Add espresso test

--- app/src/androidTest/java/com/siegmund/cleanarchitectureexample/MainActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/siegmund/cleanarchitectureexample/MainActivityInstrumentedTest.kt
@@ -1,9 +1,11 @@
 package com.siegmund.cleanarchitectureexample
 
 import android.support.test.InstrumentationRegistry.getInstrumentation
+import android.support.test.espresso.Espresso
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
 import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.contrib.RecyclerViewActions
 import android.support.test.espresso.intent.Intents
 import android.support.test.espresso.intent.Intents.intended
 import android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent
@@ -15,6 +17,7 @@ import android.support.test.runner.AndroidJUnit4
 import com.siegmund.cleanarchitectureexample.ui.credits.CreditsActivity
 import com.siegmund.cleanarchitectureexample.ui.details.DetailsActivity
 import com.siegmund.cleanarchitectureexample.ui.main.MainActivity
+import com.siegmund.cleanarchitectureexample.ui.main.MoviesAdapter
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -24,7 +27,7 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class ExampleInstrumentedTest {
+class MainActivityInstrumentedTest {
 
     @Rule @JvmField var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
 
@@ -38,6 +41,12 @@ class ExampleInstrumentedTest {
         openActionBarOverflowOrOptionsMenu(getInstrumentation().targetContext)
         onView(withText("Credits")).perform(click())
         intended(hasComponent(CreditsActivity::class.java.name))
+    }
+
+    @Test
+    fun shouldOpenDetailsScreen() {
+        onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition<MoviesAdapter.MovieViewHolder>(0, click()))
+        intended(hasComponent(DetailsActivity::class.java.name))
     }
 
     @After
